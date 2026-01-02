e829fa6a286c6ba772b8a9286bd2c9353a3a1478
Mohid2002 <muhammadmohid2110@gmail.com>
2026-01-02 18:03:38 +0500

    update2

--- travelmate/lib/homescreen/trip_model.dart
+++ travelmate/lib/homescreen/trip_model.dart
@@ -1,15 +1,22 @@
 class TripModel {
-  final String image;
   final String name;
   final String location;
+  final String image;
   final DateTime date;
+  final String history;
+  final double lat;
+  final double lon;
 
   TripModel({
-    required this.image,
     required this.name,
     required this.location,
+    required this.image,
     required this.date,
+    required this.history,
+    required this.lat,
+    required this.lon,
   });
 }
 
-List<TripModel> savedTrips = [];
+// GLOBAL LIST
+List<TripModel> savedTrips = [];  
